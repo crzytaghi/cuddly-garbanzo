93b51f734725c091fd3a92001d9d3539804cc1bc
Hannah Livnat <hannahlivnat@gmail.com>
2020-06-16 14:41:39 -0700

    added beansController to server.js file and fixed typo, express.json didn't have a ()

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// app.use(express.json);
-// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// })
+
+const beansController = require('./controllers/beans');
+app.use('/beans', beansController);
 
 mongoose.connect('mongodb://localhost:27017/beansapp', {
   useNewUrlParser: true,
